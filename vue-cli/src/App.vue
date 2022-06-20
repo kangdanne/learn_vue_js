733347ccb0637d6320a97dab14d3350b537137ff
kangdan <kangdanne@gmail.com>
2022-06-20 23:56:19 +0900

    chapture14. 싱글 파일 컴포넌트

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,31 +1,24 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-    <!-- 컴포넌트 명명법 종류 -->
+  <div>
+    <!--template의 html 내부 구조는 최상위 레벨에 하나의 태그 존재해야함-->
+    {{ str }}
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+// new Vue({
+//   data: {
+//     str: "hi",
+//   },
+// });
 
 export default {
-  // 인스턴스 옵션 속성 or 컴포넌트 옵션 속성
-  name: "App",
-  components: {
-    HelloWorld,
-    // 'hello-world': HelloWorld 와 같은 의미
+  data: function () {
+    return {
+      str: "hi",
+    };
   },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style scoped></style>
