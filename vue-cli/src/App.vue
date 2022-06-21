1dfd7a0e045022682638c74cbbc5c7b035e9a2de
kangdan <kangdanne@gmail.com>
2022-06-21 23:54:09 +0900

    chapture14. 싱글 파일 컴포넌트 - 컴포넌트 등록

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,11 +1,22 @@
 <template>
   <div>
+    <app-header></app-header>
     <!--template의 html 내부 구조는 최상위 레벨에 하나의 태그 존재해야함-->
     {{ str }}
   </div>
 </template>
 
 <script>
+import AppHeader from "./components/AppHeader.vue";
+// import시 .vue 까지 작성해야 파일을 깨지지않고 사용 할 수 있다.
+
+/* 아래와 같은 내용.
+
+var AppHeader = {
+  template: "<hㅞㅡeader><h1>Header</h1></header>",
+};
+*/
+
 // new Vue({
 //   data: {
 //     str: "hi",
@@ -17,6 +28,9 @@ export default {
     return {
       str: "hi",
     };
+  },
+  components: {
+    "app-header": AppHeader,
   },
 };
 </script>
