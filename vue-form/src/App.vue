8f8c4cf59713316ff8ebfc2c8357bcd2f012af6a
kangdan <kangdanne@gmail.com>
2022-06-30 00:27:12 +0900

    chapture15. final project

---
+++ vue-form/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <form>
+    <div>
+      <label for="username">id: </label>
+      <input id="usermane" type="text" />
+    </div>
+    <div>
+      <label for="password">pasword: </label>
+      <input id="password" type="password" />
+    </div>
+    <button>login</button>
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
