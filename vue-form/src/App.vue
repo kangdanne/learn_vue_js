5925f1fc60a9bd8dbc0b88db15ae6d421e2c804c
kangdan <kangdanne@gmail.com>
2022-07-01 23:59:01 +0900

    chapture15. final project - submit

--- vue-form/src/App.vue
+++ vue-form/src/App.vue
@@ -1,19 +1,32 @@
 <template>
-  <form>
+  <form v-on:submit="submitForm">
     <div>
       <label for="username">id: </label>
-      <input id="usermane" type="text" />
+      <input id="usermane" type="text" v-model="username" />
     </div>
     <div>
       <label for="password">pasword: </label>
-      <input id="password" type="password" />
+      <input id="password" type="password" v-model="password" />
     </div>
-    <button>login</button>
+    <button type="submit">login</button>
   </form>
 </template>
 
 <script>
-export default {};
+export default {
+  data: function () {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methode: {
+    submitForm: function (event) {
+      event.preventDefault(); //form의 이동, 새로고침을 막는 역할을 하게 됨.
+      console.log(this.username, this.password);
+    },
+  },
+};
 </script>
 
 <style scoped></style>
